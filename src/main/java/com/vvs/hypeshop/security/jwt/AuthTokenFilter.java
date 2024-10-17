88c92da89f44b45ebd45158c396d42f39bc660a7
Mehretu <mehertu.abreham@gmail.com>
2024-10-17 18:05:09 +0300

    filter added

---
+++ src/main/java/com/vvs/hypeshop/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,57 @@
+package com.vvs.hypeshop.security.jwt;
+
+import com.vvs.hypeshop.security.user.ShopUserDetailsService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.var;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+    private ShopUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String jwtToken = parseJwtToken(request);
+            if (StringUtils.hasText(jwtToken) && jwtUtils.validateToken(jwtToken)) {
+                String username = jwtUtils.getUsernameFromToken(jwtToken);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                var auth = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage() + " : Invalid or expired token, you may login and try again.");
+        }
+
+    }
+
+    public String parseJwtToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+
+    }
+
+
+
+
+}
